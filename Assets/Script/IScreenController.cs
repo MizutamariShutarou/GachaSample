528b782b84985573ca12dea357174fdf30b5eac7
MizutamariShutarou <shutaro0415@outlook.jp>
2023-05-27 02:06:54 +0900

    [add] IScreenController追加

---
+++ Assets/Script/IScreenController.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//日本語対応
+public abstract class UIController : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
