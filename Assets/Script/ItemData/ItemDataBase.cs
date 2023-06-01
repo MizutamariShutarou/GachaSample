03474fc4e8ff4a3776f7a585955538ab71dbf8a5
MizutamariShutarou <shutaro0415@outlook.jp>
2023-06-02 00:00:54 +0900

    [add] ScriptableObjectにアイコンの情報オブジェクトの情報を追加

--- Assets/Script/ItemData/ItemDataBase.cs
+++ Assets/Script/ItemData/ItemDataBase.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 //日本語対応
 public abstract class ItemDataBase : ScriptableObject
@@ -8,6 +9,10 @@ public abstract class ItemDataBase : ScriptableObject
     public string _itemName = default;
 
     public int _rareNum = default;
+
+    public Image _iconmage;
+
+    public GameObject _itemObject;
 
     public ItemType _itemType = default;
 }
